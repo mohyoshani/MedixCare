c8efd7b3b8ef7453b3c4263dd57fa0cc3b3443e7
Mohamed Yousri Hani Ali <moh.yos.hani@gmail.com>
2026-07-15 17:58:52 +0300

    Adding Profile Controller

--- MedixCare/Areas/Identity/Controllers/ProfileController.cs
+++ MedixCare/Areas/Identity/Controllers/ProfileController.cs
@@ -1,12 +1,72 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Mapster;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MedixCare.Areas.Identity.Controllers
 {
+    [Area(SD.IDENTITY_AREA)]
     public class ProfileController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public ProfileController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
-            return View();
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Update()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var result = user.Adapt<ApplicationUserVM>();
+            return View(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Update(ApplicationUserVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+            if (user is null)
+            {
+                return NotFound();
+            }
+
+            user.FullName = model.FullName;
+            user.Address = model.Address;
+
+            if (user.Email != model.Email)
+            {
+                await _userManager.SetEmailAsync(user, model.Email);
+                await _userManager.SetUserNameAsync(user, model.Email);
+            }
+
+            var result = await _userManager.UpdateAsync(user);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+                return View(model);
+            }
+
+            if (model.CurrentPassword is not null && model.NewPassword is not null)
+            {
+                    var passwordResult = await _userManager.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
+                    if (!passwordResult.Succeeded)
+                    {
+                        ModelState.AddModelError(string.Join(", ", passwordResult.Errors.Select(e => e.Description)), "Password change failed.");
+                        return View(model);
+                    }
+            }
+            await _signInManager.RefreshSignInAsync(user);
+            TempData["Success"] = "Profile and Password Updated successfully.";
+            return RedirectToAction("Update");
         }
     }
 }
---
+++ MedixCare/ViewModels/ApplicationUserVM.cs
@@ -0,0 +1,24 @@
+﻿namespace MedixCare.ViewModels
+{
+    public class ApplicationUserVM
+    {
+       
+        [Required]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+
+        [DataType(DataType.Password)]
+        public string? CurrentPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
+        public string? NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "The password and confirmation password do not match.")]
+        public string? ConfirmPassword { get; set; }
+    }
+}
